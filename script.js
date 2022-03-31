67ce71edbdf615d0549ecbab39273e9602bf0450
Jeron <norejudeisa@gmail.com>
2022-03-31 01:52:44 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+const btns = document.querySelectorAll('.btn')
+const billInput = document.querySelector('.billInput')
+const peopleInput = document.querySelector('#people')
+const customInput = document.querySelector('#custom')
+const errorMessage = document.querySelector('.errorMessage')
+const tip = document.querySelector('#tip')
+const totalTip = document.querySelector('#total')
+let numberOfPeople = null
+let amount = null
+let percent = null
+// Adding active  state to buttons
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    btns.forEach((item) => {
+      item.classList.remove('active')
+    })
+    e.target.classList.add('active')
+    percent = btn.getAttribute('data-percent')
+    calculateTip(numberOfPeople, percent, amount)
+  })
+})
+billInput.addEventListener('input', (e) => {
+  amount = e.target.value
+  calculateTip(numberOfPeople, percent, amount)
+})
+peopleInput.addEventListener('input', (e) => {
+  numberOfPeople = e.target.value
+  calculateTip(numberOfPeople, percent, amount)
+})
+customInput.addEventListener('input', (e) => {
+  btns.forEach((item) => {
+    item.classList.remove('active')
+  })
+  percent = e.target.value
+  calculateTip(numberOfPeople, percent, amount)
+})
+// Calculate Tip
+const calculateTip = (numberOfPeople, percent, amount) => {
+  if (!numberOfPeople || !amount || !percent) return
+  const tipPerPerson = ((percent / 100) * amount).toFixed(2)
+  const sumOfTip = (tipPerPerson * numberOfPeople).toFixed(2)
+  tip.textContent = tipPerPerson
+  totalTip.textContent = sumOfTip
+}
+// CHECKING FOR INVALID INPUT VALUES OR NULL
+billInput.addEventListener('blur', (e) => {
+  if (e.target.value.length === 0) {
+    billInput.classList.add('invalid')
+  } else billInput.classList.remove('invalid')
+})
+peopleInput.addEventListener('blur', (e) => {
+  if (e.target.value.length === 0) {
+    peopleInput.classList.add('invalid')
+    errorMessage.style.display = 'block'
+  } else {
+    peopleInput.classList.remove('invalid')
+    errorMessage.style.display = 'none'
+  }
+})
